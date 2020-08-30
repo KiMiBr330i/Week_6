60295d114680d118b16c8274bf4c0b71b5a3a724
KiMiBr330i <68255276+KiMiBr330i@users.noreply.github.com>
2020-08-30 16:23:15 -0600

    commit 08-30-20 1623hrs

--- Week_6_1/src/Lab6_Ex5.java
+++ Week_6_1/src/Lab6_Ex5.java
@@ -34,3 +34,15 @@ public class Lab6_Ex5 {
 	}
 
 }
+//For the insertion sort, the for loop iterates through each index of the array starting with index value 1.
+//The while loop compares the element specified by the for loop to the element at the index previous to it.
+//If the difference between the current and preceding elements is less than zero, the latter element is inserted
+//to the prior element's index. That forces a shift downward of the prior element to accommodate the insertion.
+//In this case: for i=1: the difference between 1 and 29 is larger than zero, so 1 stays, and 29 is inserted in place.
+//Array is unchanged: {1,29,14,15,94}
+//For i=2: Comparison of 14 and 29 yields an insertion of 14 after 1, and a shift of 29 downward.
+//New array sort: {1,14,29,15,94}
+//For i=3: Comparison of 15 and 29 yields a similar result as i=2
+//New array sort: {1,14,15,29,94}
+//For i = 4: 94 is at the end of the array and cannot be compared to a next element. It is sorted and inserted in place.
+//Final result is {1,14,15,29, 94}
